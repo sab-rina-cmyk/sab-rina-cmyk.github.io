bdc8b9aedaa66dd707862907e46ca2a66becc922
sab-rina-cmyk <sabrinah@ocadu.ca>
2025-04-25 23:16:52 -0400

    Final Project Commit
    Final Project Commit

---
+++ sabrinahportfolio/sketch.js
@@ -0,0 +1,69 @@
+// adding new fonts into my document: https://helpx.adobe.com/ca/fonts/using/css-selectors.html
+
+// header animation remix reference: https://happycoding.io/tutorials/p5js/for-loops/spooky-text
+// START //
+let header = ['SABRINA', 'HUFFMAN'];
+let spans = [];
+const ySpeed = 0.1;
+const amplitude = 15;
+const verticalLetterSpacing = 5;
+let isHovered = false;
+
+function setup() {
+    noCanvas();
+    const container = select('#wiggle-header');
+  
+    header.forEach((word, index) => {
+      const wordSpan = createSpan();
+      wordSpan.parent(container);
+      wordSpan.style('display', 'inline-block');
+      wordSpan.style('margin', '0 1rem');
+      wordSpan.class('word-group');
+  
+      for (let i = 0; i < word.length; i++) {
+        let letterSpan = createSpan(word[i]);
+        letterSpan.parent(wordSpan);
+        letterSpan.style('display', 'inline-block');
+        letterSpan.style('font-size', '6rem');
+        letterSpan.style('font-family', 'puffin-display-soft, sans-serif');
+        letterSpan.style('color', '#333');
+        letterSpan.style('font-size', '10rem');
+        spans.push(letterSpan);
+      }
+  
+      if (index === 0) {
+        const br = createElement('br');
+        br.parent(container);
+      }
+    });
+
+  container.mouseOver(() => isHovered = true);
+  container.mouseOut(() => {
+    isHovered = false;
+    resetPositions();
+  });
+
+ const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+      }
+    });
+  });
+
+  document.querySelectorAll('.animate-on-scroll').forEach(el => {
+    observer.observe(el);
+  });
+}
+
+function draw() {
+  if (isHovered) {
+    for (let i = 0; i < spans.length; i++) {
+      const offset = i * verticalLetterSpacing;
+      const y = sin((frameCount - offset) * ySpeed) * amplitude;
+      spans[i].style('transform', `translateY(${y}px)`);
+    }
+  }
+}
+// // END //
+// header animation remix reference: https://happycoding.io/tutorials/p5js/for-loops/spooky-text
